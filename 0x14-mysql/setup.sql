13b67e1c5e92d5d660a273adefa83522fcb3203f
Amodu Olalekan <olalekzy98@gmail.com>
2024-04-16 00:40:11 -0700

    setup.sql

---
+++ 0x14-mysql/setup.sql
@@ -0,0 +1,12 @@
+CREATE USER Olalekzy_user@localhost IDENTIFIED BY "projectcorrection280hbtn";
+GRANT REPLICATION CLIENT ON *.* TO 'Olalekzy_user'@'localhost';
+CREATE DATABASE tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE nexus6(id INTEGER, name TEXT);
+INSERT INTO nexus6 VALUES (0, "Jarvis");
+GRANT SELECT ON tyrell_corp.nexus6 TO Olalekzy_user@localhost;
+CREATE USER replica_user@'%' IDENTIFIED BY "replica_user";
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO Olalekzy_user@localhost;
+CREATE USER web02@100.26.138.5 IDENTIFIED BY "web02";
+GRANT REPLICATION SLAVE ON *.* TO web02@100.26.138.5;
